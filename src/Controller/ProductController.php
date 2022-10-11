38b1232b5c1f7f06bcc167a610a6ab42aaee929e
LudovicBraine <ludovic.braine@gmail.com>
2022-10-11 17:03:42 +0200

    add product view

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    #[Route('/product', name: 'app_product')]
+    public function index(ProductRepository $productRepository): Response
+    {
+        $products = $productRepository->findAll();
+
+        return $this->render('product/index.html.twig', [
+            'products' => $products
+        ]);
+    }
+}
