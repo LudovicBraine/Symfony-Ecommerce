a75b18c30cf5086575c0bfeaa1352af1c9bd256a
LudovicBraine <ludovic.braine@gmail.com>
2022-11-10 23:52:06 +0100

    finish product page

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -9,13 +9,27 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
+    #[Route('/products', name: 'app_products')]
     public function index(ProductRepository $productRepository): Response
     {
         $products = $productRepository->findAll();
 
         return $this->render('product/index.html.twig', [
             'products' => $products
+        ]);
+    }
+
+    #[Route('/product/{slug}', name: 'app_product')]
+    public function show(ProductRepository $productRepository, string $slug): Response
+    {
+        $product = $productRepository->findOneBy(['slug' => $slug]);
+
+        if (!$product) {
+            return $this->redirectToRoute('app_products');
+        }
+
+        return $this->render('product/show.html.twig', [
+            'product' => $product
         ]);
     }
 }
