8ad6293ba79f30526d9bef74503f6817897f1a05
LudovicBraine <ludovic.braine@gmail.com>
2022-11-11 21:13:07 +0100

    add filter on product + cart

--- src/Classe/Cart.php
+++ src/Classe/Cart.php
@@ -3,14 +3,17 @@
 namespace App\Classe;
 
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use App\Repository\ProductRepository;
 
 class Cart
 {
     private $session;
+    private $productRepository;
 
-    public function __construct(SessionInterface $session)
+    public function __construct(SessionInterface $session, ProductRepository $productRepository)
     {
         $this->session = $session;
+        $this->productRepository = $productRepository;
     }
 
     public function add($id)
@@ -34,5 +37,47 @@ class Cart
     public function remove()
     {
         $this->session->remove('cart');
+    }
+
+    public function removeOne($id)
+    {
+        $cart = $this->session->get('cart');
+
+        unset($cart[$id]);
+
+        return $this->session->set('cart', $cart);
+    }
+
+    public function decrease($id)
+    {
+        $cart = $this->session->get('cart', []);
+        if ($cart[$id] > 1) {
+            $cart[$id]--;
+        } else {
+            unset($cart[$id]);
+        }
+
+        return $this->session->set('cart', $cart);
+    }
+
+    public function getAll()
+    {
+        $cartComplete = [];
+
+        foreach ($this->get() as $id => $quantity) {
+            $productObject =  $this->productRepository->findOneBy(['id' => $id]);
+
+            if (!$productObject) {
+                $this->removeOne($id);
+                continue;
+            }
+
+            $cartComplete[] = [
+                'product' => $productObject,
+                'quantity' => $quantity
+            ];
+        }
+
+        return $cartComplete;
     }
 }
--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Classe\Cart;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,17 +11,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class CartController extends AbstractController
 {
     #[Route('/cart', name: 'cart')]
-    public function index(Cart $cart): Response
+    public function index(Cart $cart, ProductRepository $productRepository): Response
     {
-        dd($cart->get());
-        return $this->render('cart/index.html.twig');
+        return $this->render('cart/index.html.twig', [
+            'cart' => $cart->getAll()
+        ]);
     }
 
     #[Route('/cart/add/{id}', name: 'add_to_cart')]
     public function add(Cart $cart, $id): Response
     {
         $cart->add($id);
-        return $this->redirectToRoute('cart');
+        return $this->redirectToRoute('app_products');
     }
 
     #[Route('/cart/remove', name: 'remove_cart')]
@@ -28,5 +30,19 @@ class CartController extends AbstractController
     {
         $cart->remove();
         return $this->redirectToRoute('app_products');
+    }
+
+    #[Route('/cart/remove/{id}', name: 'remove_one')]
+    public function removeOne(Cart $cart, $id): Response
+    {
+        $cart->removeOne($id);
+        return $this->redirectToRoute('cart');
+    }
+
+    #[Route('/cart/decrease/{id}', name: 'decrease')]
+    public function decreaseItem(Cart $cart, $id): Response
+    {
+        $cart->decrease($id);
+        return $this->redirectToRoute('cart');
     }
 }
