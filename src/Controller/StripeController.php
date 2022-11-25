a1d3936ed49d2e74cde794d0eb1091d9849a319a
LudovicBraine <ludovic.braine@gmail.com>
2022-11-25 23:13:57 +0100

    add list to create a stripe product

--- src/Controller/StripeController.php
+++ src/Controller/StripeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,19 +12,25 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 class StripeController extends AbstractController
 {
 
-    public function __construct(HttpClientInterface $client)
+    public function __construct(HttpClientInterface $client, ProductRepository $productRepository)
     {
         $this->client = $client;
+        $this->productRepository = $productRepository;
     }
 
     #[Route('/stripe', name: 'app_stripe')]
     public function index(): Response
     {
+
+        $products = $this->productRepository->findAll();
+
         return $this->render('stripe/index.html.twig', [
             'controller_name' => 'StripeController',
+            'products' => $products
         ]);
     }
 
+    #[Route('/stripe/add', name: 'app_stripe_add')]
     public function add(): Response
     {
         $response = $this->client->request(
